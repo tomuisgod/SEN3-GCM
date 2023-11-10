977d111c27cef4f0662ffe2041c41d113632743c
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-11-10 13:35:55 +0100

    Update vlocka_rekurzia.py

--- vlocka_rekurzia.py
+++ vlocka_rekurzia.py
@@ -2,13 +2,15 @@
 
 import turtle
 
+t = turtle.Turtle()
+
 def rek(n, d):
     for i in range(6):        
-        forward(d)
+        t.forward(d)
         if n > 0:
 
             rek(n-1, d/2.5)
-        back(d)
-        right(360/6)
+        t.back(d)
+        t.right(360/6)
     
 rek(1, 100)
