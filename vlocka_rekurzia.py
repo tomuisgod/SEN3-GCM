660c0314bc6c50309ef73bf929d656a9db8296d0
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-11-10 13:11:47 +0100

    Update vlocka_rekurzia.py

--- vlocka_rekurzia.py
+++ vlocka_rekurzia.py
@@ -1,2 +1,14 @@
 ## viemeprogramovat.sk rekurzia priklad
 
+import turtle
+
+def rek(n, d):
+    for i in range(6):        
+        forward(d)
+        if n > 0:
+
+            rek(n-1, d/2.5)
+        back(d)
+        right(360/6)
+    
+rek(1, 100)
