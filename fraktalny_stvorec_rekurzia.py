de0f1984c6de5a86dc4d4305c493ec50f6009581
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-11-10 14:10:08 +0100

    Update fraktalny_stvorec_rekurzia.py

--- fraktalny_stvorec_rekurzia.py
+++ fraktalny_stvorec_rekurzia.py
@@ -1,2 +1,33 @@
 # zadanie z viemeprogramovat(alebo vieme inf) na rekurziu
 
+import turtle
+
+t = turtle.Turtle()
+
+def stvorec_rek(n):
+    # for i in range(4):
+    t.forward(100/2**(n-1))
+    t.left(90)
+    if n > 3:
+        stvorec_rek(n+1)
+    t.right(180)
+
+    t.forward(100/2**(n-1))
+    t.left(90)
+    if n > 3:
+        stvorec_rek(n+1)
+    t.right(180)
+
+    t.forward(100/2**(n-1))
+    t.left(90)
+    if n > 3:
+        stvorec_rek(n+1)
+    t.right(180)
+
+    t.forward(100/2**(n-1))
+    t.left(90)
+    if n > 3:
+        stvorec_rek(n+1)
+    t.right(180)
+
+stvorec_rek(1)
