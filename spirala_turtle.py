b89497cc40430ffb0dc98f7f397b230a9caeb0d9
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2024-01-19 13:59:42 +0100

    Update spirala_turtle.py

--- spirala_turtle.py
+++ spirala_turtle.py
@@ -4,13 +4,28 @@ import random
 col = ["red", "blue", "yellow", "green", "orange", "pink", "purple", "grey", "black"]
 
 t = turtle.Turtle()
-a = True
-t.pensize(4)
 
-while a is True:
-    for i in range(3, 300, 3):
-        t.pencolor(random.choice(col))
-        t.forward(i)
-        t.rt(90)
-    a = False
+def spirala_stvorec():
+
+    t.pensize(4)
+    a = True
+
+    while a is True:
+        for i in range(3, 300, 3):
+            t.pencolor(random.choice(col))
+            t.forward(i)
+            t.rt(90)
+        a = False
+
+
+def divna_spirala():
+    turtle.delay(0)
+    for uhol in range(1, 2000):
+        t.forward(8)
+        t.right(uhol + 0.1)
+
+
+divna_spirala()
+
+spirala_stvorec()
 
