2b3c849eee171825ab36e8d925ab3fac4e6d4934
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2024-04-19 13:42:16 +0200

    Update macka_sifra.py

--- macka_sifra.py
+++ macka_sifra.py
@@ -1,1 +1,20 @@
+# Ascii posun sifra
 
+retazec = "ahoj svet".lower()
+heslo = "macka".lower()
+
+nretazec = []
+
+i = 0
+for char in retazec:
+    ascii_shift = 122-ord(heslo[i%len(heslo)])
+    if ord(char) + ascii_shift >122:
+        ascii_shift = 122-ord(char) - ascii_shift
+    nretazec.append(chr(ord(char)+ascii_shift))
+    i += 1
+
+retazec = ""
+for ch in nretazec:
+    retazec += ch
+
+print(retazec) # nacy9mack
